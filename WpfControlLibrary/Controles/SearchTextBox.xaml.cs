6e4d390eaa53abbe553537d339f746f1343b031c
Dmytro Lamashevski <56080362+DmytroLamashevskyi@users.noreply.github.com>
2020-11-22 21:24:12 +0100

    Add project files.

---
+++ WpfControlLibrary/Controles/SearchTextBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlLibrary.Controles
+{
+    /// <summary>
+    /// Interaction logic for SearchTextBox.xaml
+    /// </summary>
+    public partial class SearchTextBox : UserControl
+    {
+        public string SearchString { set; get; }
+
+        public delegate void  Search(string arg);
+        public event Search SearchClick;
+
+        public SearchTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SearchClick?.Invoke(SearchString); 
+        }
+    }
+}
